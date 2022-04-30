c12628aab56dfbb7e6672b0a7156c6dd8a0124c6
Josh Baker <josh.jacob.baker@gmail.com>
2022-04-30 09:43:26 -0700

    Added list of react router dom routes within overlay

--- src/js/layout/HomePage/Modal/ModalOverlay.js
+++ src/js/layout/HomePage/Modal/ModalOverlay.js
@@ -2,12 +2,27 @@ import react from "react"
 import ReactDOM from "react-dom"
 import { useLocation } from "react-router-dom"
 import ModalOverlayStyled from "./ModalOverlayStyled"
-// import NavLinkStyled from "../../../components/navLink/NavLinkStyled"
+import NavLinkStyled from "../../../components/navLink/NavLinkStyled"
 
 const ModalOverlay = (props) => {
   const { pathname } = useLocation()
   // return ReactDOM.createPortal(<ModalOverlayStyled />, document.getElementById("modal"))
-  return ReactDOM.createPortal(<ModalOverlayStyled></ModalOverlayStyled>, document.getElementById("modalOverlay"))
+  return ReactDOM.createPortal(
+    <ModalOverlayStyled>
+      <ul>
+        <li>
+          <NavLinkStyled to="/">Route /</NavLinkStyled>
+        </li>
+        <li>
+          <NavLinkStyled to="/counter">Route /counter</NavLinkStyled>
+        </li>
+        <li>
+          <NavLinkStyled to="/addpostform">Route /addpostform</NavLinkStyled>
+        </li>
+      </ul>
+    </ModalOverlayStyled>,
+    document.getElementById("modalOverlay")
+  )
 }
 
 export default ModalOverlay
